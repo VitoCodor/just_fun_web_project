168d26688c63c76f01722a308091fe602a559dc8
Vito <laiwenbin_gd@163.com>
2020-06-21 18:33:42 +0800

    init, and add some code

---
+++ src/main/java/com/vitoboy/funweb/FunWithWebApplication.java
@@ -0,0 +1,13 @@
+package com.vitoboy.funweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunWithWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FunWithWebApplication.class, args);
+    }
+
+}
